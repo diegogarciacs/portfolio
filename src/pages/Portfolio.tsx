054e28c9d3a632260d1b4d60090ed4789a7955eb
Diego O Garcia <diegogar577@gmail.com>
2023-01-24 15:06:44 -0800

    fixes for card view

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,13 +3,13 @@ import CardList from "../components/Portfolio/CardList";
 export default function Portfolio() {
   return (
     <>
-      <div className='min-h-64 bg-neutral'>
-        <section className='mx-auto flex w-1/2 justify-center text-center'>
-          <div className='layout py-6 text-center font-bold text-5xl'>
+      <div className="min-h-64 bg-neutral">
+        <section className="mx-auto flex w-1/2 justify-center text-center">
+          <div className="layout py-6 text-center font-bold text-5xl">
             <h1>Some Things I've Built.</h1>
           </div>
         </section>
-        <div className='cardlist_container flex justify-center'>
+        <div className="cardlist_container flex flex-wrap justify-center">
           <CardList />
         </div>
       </div>
