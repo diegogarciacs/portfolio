ab09eafffadf8770688cd023819261141a698fb2
Diego O Garcia <diegogar577@gmail.com>
2023-02-06 09:40:48 -0800

    updates for safari/mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="hero min-h-screen	 bg-neutral" id="home">
         <div className="hero-content flex-col gap-[2rem] lg:flex-row">
           <div className="person relative w-60 sm:w-auto lg:mt-[-6rem]">
-            <div className="person_container ease  group grid aspect-[1/1.35] items-end overflow-hidden rounded-b-[100vw] transition duration-[var(--transform-speed)] hover:scale-[1.1]">
+            <div className="person_container ease  group grid sm:aspect-[1/1.35] items-end overflow-hidden rounded-b-[100vw] transition duration-[var(--transform-speed)] hover:scale-[1.1]">
               <div
                 className="person_circle ease duration-[calc(var(--transform-speed) * 1.2)] h-55 w-50 m-h-48 m-w-48
               group absolute inset-x-0 bottom-0 top-auto bottom-0 m-auto aspect-square rounded-full bg-gradient-to-bl
@@ -14,7 +14,7 @@ export default function Home() {
               transition group-hover:from-[#1F95D6] group-hover:to-[#17E882]"
               ></div>
               <img
-                className="person_img z-2 shadow-3xl group-hover:scale[1.1] ease duration-[calc(var(--transform-speed) *	1.12)] pointer-events-none relative left-[1rem] scale-[1.2] transition group-hover:translate-y-[-1.4rem] sm:w-[20rem] lg:w-[25rem]"
+                className="person_img z-2 shadow-3xl group-hover:scale[1.1] ease duration-[calc(var(--transform-speed) *	1.12)] pointer-events-none relative top-[2rem] pt-6 sm:pt-0 sm:top-[0rem] left-[1rem] scale-[1.2] transition group-hover:translate-y-[-1.4rem] sm:w-[20rem] lg:w-[25rem]"
                 src="/portfolio/images/diego-transparent.png"
                 alt="diego smiling"
               />
