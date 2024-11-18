4b8eb717891ce57cf46020d4ab6d74d540b52585
Diego O Garcia <diegogar577@gmail.com>
2024-11-17 20:16:48 -0800

    Remove BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
